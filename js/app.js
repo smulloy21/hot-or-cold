c7da73584477e1574038cd106c78e09cf204079d
Shannon Mulloy <smulloy21@gmail.com>
2015-07-16 17:22:36 -0500

    js runs

--- js/app.js
+++ js/app.js
@@ -2,43 +2,76 @@ var getNum = function() {
 	return Math.floor((Math.random() * 100) + 1);
 };
 
+var addGuess = function(g){
+	$("#count").text(g);
+};
+
 var newGame = function(){
-	var num = getNum();
-	alert(num);
 	var guessCount = 0;
+	var y = '';
+	var num = getNum();
 	$("#guessButton").on("click", function(e){
 		e.preventDefault();
 		var x = $("#userGuess").val();
-		$("#userGuess").val('');
-		if (x >= num + 10 || x <= num - 10 ) {
-			alert("Cold!");
+
+		if (x % 1 === 0 && x !== '') {
+			if (x == num) {
+				guessCount++;
+				addGuess(guessCount);
+				$("#feedback").text("Awesome! That's the number!");
+				$("#userGuess").val('');
+				y = x;
+			} else if (x <= num + 2 && x >= num - 2 ) {
+				$("#feedback").text("Burning Hot!");
+				$("#userGuess").val('');
+				y = x;
+			} else if (y == '' || Math.abs(num-x) == Math.abs(num-y)) {
+				guessCount++;
+				addGuess(guessCount);
+				if (x >= num + 10 || x <= num - 10 ) {
+					$("#feedback").text("Cold!");
+				} else {
+					$("#feedback").text("Hot!");
+				}
+				$("#userGuess").val('');
+				y = x;
+			} else if (Math.abs(num-x) < Math.abs(num-y)) {
+				guessCount++;
+				addGuess(guessCount);
+				$("#feedback").text("Warmer!");
+				$("#userGuess").val('');
+				y = x;
+			} else if (Math.abs(num-y) < Math.abs(num-x)) {
+				guessCount++;
+				addGuess(guessCount);
+				$("#feedback").text("Colder!");
+				$("#userGuess").val('');
+				y = x;
+			}
 		} else {
-			alert("Hot!");
+			$("#userGuess").val('');
 		}
-		guessCount++;
-		$("#count").text(guessCount);
+			
 	});
-	console.log(num);
 };
 
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
 	$(".what").click(function(){
-  	$(".overlay").fadeIn(1000);
+  	$(".overlay").fadeIn(400);
 
 	});
 
 	/*--- Hide information modal box ---*/
 	$("a.close").click(function(){
-		$(".overlay").fadeOut(1000);
+		$(".overlay").fadeOut(400);
 	});
 
 	newGame();
 
-});
+	$("nav .new").on("click", function(){
+		location.reload();
+	});
 
-	// $("#userGuess").on("keypress", "input", function() {
-	// 	var guess = $(this).val('');
-	// 	alert(guess);
-	// });
+});
